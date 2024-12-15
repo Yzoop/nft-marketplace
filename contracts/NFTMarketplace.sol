f553342a16470a48abdcf87a8145d6e6eedb720d
Ivan Kalinchuk <iv.ikalin@gmail.com>
2024-12-15 21:00:57 +0100

    Added Auction Functionality

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -56,7 +56,95 @@ contract NFTMarketplace is ERC721URIStorage, Ownable {
         emit NFTSold(tokenId, msg.sender);
     }
 
+    async function fetchNFTs(contract) {
+            const totalSupply = await contract.tokenCount();
+            let nftList = [];
+
+            for (let i = 1; i <= totalSupply; i++) {
+            const nft = await contract.nfts(i);
+            nftList.push({
+                tokenId: i,
+                seller: nft.seller,
+                owner: nft.owner,
+                price: ethers.utils.formatEther(nft.price),
+                listed: nft.listed,
+            });
+        }
+    }
+
+  return nftList.filter(nft => nft.listed); // Show only listed NFTs
+}
+
     function withdrawListingFee() public onlyOwner {
         payable(owner()).transfer(address(this).balance);
     }
+
+    struct Auction {
+    uint256 tokenId;
+    address payable seller;
+    uint256 startingPrice;
+    uint256 highestBid;
+    address payable highestBidder;
+    uint256 endTime;
+    bool ended;
+}
+
+    mapping(uint256 => Auction) public auctions;
+
+    event AuctionCreated(uint256 indexed tokenId, uint256 startingPrice, uint256 endTime);
+    event NewBid(uint256 indexed tokenId, address bidder, uint256 bidAmount);
+    event AuctionEnded(uint256 indexed tokenId, address winner, uint256 winningBid);
+
+    function createAuction(uint256 tokenId, uint256 startingPrice, uint256 duration) public {
+        require(msg.sender == nfts[tokenId].owner, "Only the owner can create an auction");
+        require(nfts[tokenId].listed == false, "NFT must not be listed for sale");
+        require(duration > 0, "Duration must be greater than zero");
+
+        auctions[tokenId] = Auction(
+            tokenId,
+            payable(msg.sender),
+            startingPrice,
+            0,
+            payable(address(0)),
+            block.timestamp + duration,
+            false
+        );
+
+        emit AuctionCreated(tokenId, startingPrice, block.timestamp + duration);
+    }
+
+    function placeBid(uint256 tokenId) public payable {
+        Auction storage auction = auctions[tokenId];
+        require(block.timestamp < auction.endTime, "Auction has ended");
+        require(msg.value > auction.highestBid, "Bid must be higher than the current highest bid");
+
+        if (auction.highestBid > 0) {
+            // Refund the previous highest bidder
+            auction.highestBidder.transfer(auction.highestBid);
+        }
+
+        auction.highestBid = msg.value;
+        auction.highestBidder = payable(msg.sender);
+
+        emit NewBid(tokenId, msg.sender, msg.value);
+    }
+
+    function endAuction(uint256 tokenId) public {
+        Auction storage auction = auctions[tokenId];
+        require(block.timestamp >= auction.endTime, "Auction is still ongoing");
+        require(!auction.ended, "Auction has already ended");
+
+        auction.ended = true;
+
+        if (auction.highestBid > 0) {
+            auction.seller.transfer(auction.highestBid);
+            nfts[tokenId].owner = auction.highestBidder;
+            nfts[tokenId].listed = false;
+
+            _transfer(auction.seller, auction.highestBidder, tokenId);
+        }
+
+        emit AuctionEnded(tokenId, auction.highestBidder, auction.highestBid);
+    }
+
 }
